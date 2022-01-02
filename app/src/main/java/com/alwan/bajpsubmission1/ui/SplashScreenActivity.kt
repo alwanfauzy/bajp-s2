73e1e2177b9dc96466529d64de75ef2b55e62197
Alwan Fauzi <alwanfauzi13@gmail.com>
2022-01-02 21:57:14 +0700

    Implement reviewer's suggestions

--- app/src/main/java/com/alwan/bajpsubmission1/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/alwan/bajpsubmission1/ui/SplashScreenActivity.kt
@@ -2,16 +2,17 @@ package com.alwan.bajpsubmission1.ui
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import com.alwan.bajpsubmission1.databinding.ActivitySplashScreenBinding
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
     private var _binding : ActivitySplashScreenBinding? = null
     private val binding get() = _binding!!
+    private val splashDelay = 1000L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +23,7 @@ class SplashScreenActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 1000)
+        }, splashDelay)
     }
 
     override fun onDestroy() {
